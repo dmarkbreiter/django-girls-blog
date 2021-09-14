610cd6b19c11382ac37a2924f5195da0875da3a5
dmarkbreiter <dmarkbreiter@nhm.org>
2021-09-14 16:57:52 -0700

    Django Girls Tutorial

---
+++ django-venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dmarkbreiter/Documents/first-django-project/django-venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
